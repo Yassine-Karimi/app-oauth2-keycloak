4a9b392de05c2601cb276d254eed43bbf9278a26
Yassine Karimi <yassinkarimi.1.23@gmail.com>
2023-12-04 11:22:11 +0100

    Initial commit

---
+++ ebank-service-unsecured/src/main/java/org/sid/ebankservice/web/EBankRestController.java
@@ -0,0 +1,29 @@
+package org.sid.ebankservice.web;
+
+import org.sid.ebankservice.dto.CurrencyTransferResponse;
+import org.sid.ebankservice.dto.NewWalletTransferRequest;
+import org.sid.ebankservice.entities.CurrencyDeposit;
+import org.sid.ebankservice.service.EBankServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+public class EBankRestController {
+    @Autowired
+    private EBankServiceImpl eBankService;
+    @PostMapping("/currencyTransfer")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public CurrencyTransferResponse currencyTransfer(@RequestBody NewWalletTransferRequest request){
+        return this.eBankService.newWalletTransaction(request);
+    }
+
+    @GetMapping("/currencyDeposits")
+    @PreAuthorize("hasAuthority('USER')")
+    public List<CurrencyDeposit> currencyDepositList(){
+        return eBankService.currencyDeposits();
+    }
+}
